65ad97ff1d207b97ba6fb45f89ae870d4eb51f2a
Alice Kim <gyuheekim@Alice-Kims-MBP.home>
2020-06-29 20:03:18 -0400

    Removing script tag in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
-<script>
 const btnScrollToTop = document.querySelector("#btnScrollToTop");
 btnScrollToTop.addEventListener("click", function () {
 
   $("html, body").animate({ scrollTop: 0 }, "slow");
 });
-</script>
